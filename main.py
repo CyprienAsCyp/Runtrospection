4e63fd392509d8eca5380340e3800d719b7bf077
Cyprien Calmels <cyprien.calmels@veolia.com>
2024-03-24 19:18:41 +0100

    add file uploader

--- main.py
+++ main.py
@@ -15,8 +15,17 @@ def main():
     app = StravaApp()
     auth = Authenticator(app=app)
     rts = Runtrospection()
+
+    uploaded_file = st.file_uploader(
+        "Choose a JSON file", type="json", accept_multiple_files=False
+    )
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.read()
+        st.write("Your database's filename:", uploaded_file.name)
+
     app.populate_user_activities()
     activity_id = streamlit_app.enter_activity_id()
+
     if activity_id:
         df_speed = rts.convert_activity_to_df(
             activity=app.get_laps_from_raw_activity(id=activity_id)
